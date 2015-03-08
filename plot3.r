b3599fe9fdc7dc756632f0db329004a827b13bf8
FunOnTheUpfield <bikenerd@gmail.com>
2015-03-08 16:14:05 +1100

    four scripts, four plots (and a lot of junk)

--- plot3.r
+++ plot3.r
@@ -15,31 +15,25 @@ if(!file.exists('./household_power_consumption.txt')){
   unzip('./data/electricity.zip')
   rm(fileurl)
 }
-# TODO: Write a check to see if the file is too big for the host machine as per instructions.
 
 
 # import as a R data frame  
 
-txtelectricity <- read.table('./household_power_consumption.txt', header = TRUE, sep=";", na.strings="?", nrows = 100)
-classes <- sapply(txtelectricity, class)
-electricity <- read.table('./household_power_consumption.txt', header = TRUE, sep=";",na.strings="?", colClasses=classes)
-rm(txtelectricity, classes)
+electricity <- read.table('./household_power_consumption.txt', header=FALSE, skip=66637,nrow=2880, sep=";", na.strings="?",
+                          col.names=c('Date', 'Time', 'Global_active_power', 'Global_reactive_power','Voltage','Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3'))
 
 # Convert Date and Time into POSIX values
 electricity$datetime <- strptime(paste(electricity$Date, electricity$Time), "%d/%m/%Y %H:%M:%S")
-
-# Subset electricity consumption for dates 2007-02-01 and 2007-02-02.
-plotelectricity <- electricity[electricity$datetime <= '2007-02-02' & electricity$datetime >= '2007-02-01', ]
 
 
 
 # Plot 3 Sub Metering
 png(file = 'plot3.png', width = 480, height = 480, units = "px")
-with(plotelectricity, 
-     plot(plotelectricity$datetime, plotelectricity$Sub_metering_1, type = "l", col="black", 
+with(electricity, 
+     plot(electricity$datetime, electricity$Sub_metering_1, type = "l", col="black", 
           xlab = "", ylab = "Energy sub metering"))
-     lines(plotelectricity$datetime, plotelectricity$Sub_metering_2, type = "l", col="red")
-     lines(plotelectricity$datetime, plotelectricity$Sub_metering_3, type = "l", col="blue")
+     lines(electricity$datetime, electricity$Sub_metering_2, type = "l", col="red")
+     lines(electricity$datetime, electricity$Sub_metering_3, type = "l", col="blue")
 legend("topright", lty=1, col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
--- plot4.r
+++ plot4.r
@@ -1,4 +1,5 @@
 #plot4.r
+
 # Creates the array of graphs.
 
 if(!file.exists("data")){
@@ -15,34 +16,31 @@ if(!file.exists('./household_power_consumption.txt')){
   unzip('./data/electricity.zip')
   rm(fileurl)
 }
-# TODO: Write a check to see if the file is too big for the host machine as per instructions.
 
 
 # import as a R data frame  
 
-txtelectricity <- read.table('./household_power_consumption.txt', header = TRUE, sep=";", na.strings="?", nrows = 100)
-classes <- sapply(txtelectricity, class)
-electricity <- read.table('./household_power_consumption.txt', header = TRUE, sep=";",na.strings="?", colClasses=classes)
-rm(txtelectricity, classes)
+electricity <- read.table('./household_power_consumption.txt', header=FALSE, skip=66637,nrow=2880, sep=";", na.strings="?",
+                          col.names=c('Date', 'Time', 'Global_active_power', 'Global_reactive_power','Voltage','Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3'))
 
 # Convert Date and Time into POSIX values
 electricity$datetime <- strptime(paste(electricity$Date, electricity$Time), "%d/%m/%Y %H:%M:%S")
-
-# Subset electricity consumption for dates 2007-02-01 and 2007-02-02.
-plotelectricity <- electricity[electricity$datetime <= '2007-02-02' & electricity$datetime >= '2007-02-01', ]
-
 
 
 
 # Plot 4 the array of graphs
 png(file = 'plot4.png', width = 480, height = 480, units = "px")
-par(mfrow = c(2,2))
-with(plotelectricity, {
-  plot(Voltage, type = "l")
-  plot(Global_active_power, type = "l", 
-       ylab = "Global Active Power \\(kilowatts\\)")
+par(mfrow = c(2, 2))
+with(electricity, plot(datetime, Global_active_power, type='l', 
+                             xlab="", ylab="Global Active Power (kilowatts)"))
+with(electricity, plot(datetime, Voltage, type = "l", xlab= "datetime", ylab="Voltage"))
+
+with(electricity, plot(datetime, Sub_metering_1, type = "l", col="black", 
+       xlab = "", ylab = "Energy sub metering"))
+lines(electricity$datetime, electricity$Sub_metering_2, type = "l", col="red")
+  lines(electricity$datetime, electricity$Sub_metering_3, type = "l", col="blue")
+  legend("topright", lty=1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
   
-  plot(Sub_metering_1, type = "l")
-  plot(Global_reactive_power, type = "l")
-})
+with(electricity,plot(datetime,Global_reactive_power, type = "l", xlab="datetime"))
 dev.off()
